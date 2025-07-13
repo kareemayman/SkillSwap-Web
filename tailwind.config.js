d155ca57829b0632e790ee596ce631edc1f6a851
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2025-07-13 10:26:54 +0300

    chore(downgrade tailwind & install flowbite-react)

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}", // ← include Flowbite
+    "./node_modules/flowbite/**/*.js" // ← include Flowbite core
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin') // ← add Flowbite plugin
+  ],
+};
