dea1791470ad42fc2f925bc3cf1125ef739ad910
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2025-07-28 16:24:12 +0300

    feat(dashboard users table ui desktop static data)

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,91 @@
+import { useState } from "react"
+import Avat from "../../assets/images/avat.png"
+
+export default function Dashboard() {
+  const [selectedTab, setSelectedTab] = useState("users")
+
+  return (
+    <div className="container mx-auto px-16 pt-8 pb-8">
+      <div className="flex gap-12 items-center border-b-2 border-[var(--color-card-border)] mb-8">
+        <p
+          className={`pb-4 relative ${
+            selectedTab == "users"
+              ? "text-[var(--color-text-light)]"
+              : "text-[var(--color-text-primary)]"
+          } cursor-pointer font-bold hover:text-[var(--color-text-light)] transition-all duration-300 text-lg before:absolute before:content-[""] ${
+            selectedTab == "users" && "before:w-full"
+          } before:h-[3px] before:rounded-xl before:bg-[var(--color-text-light)] before:top-[calc(100%-2px)] before:left-0 before:transition-all before:duration-300`}
+          onClick={() => setSelectedTab("users")}
+        >
+          Users
+        </p>
+        <p
+          className={`pb-4 relative ${
+            selectedTab == "skills"
+              ? "text-[var(--color-text-light)]"
+              : "text-[var(--color-text-primary)]"
+          } cursor-pointer font-bold hover:text-[var(--color-text-light)] text-lg before:absolute before:content-[""] ${
+            selectedTab == "skills" && "before:w-full"
+          } before:h-[3px] before:rounded-xl before:bg-[var(--color-text-light)] before:top-[calc(100%-2px)] before:left-0 before:transition-all before:duration-300`}
+          onClick={() => setSelectedTab("skills")}
+        >
+          Skills
+        </p>
+        <p
+          className={`pb-4 relative ${
+            selectedTab == "reviews"
+              ? "text-[var(--color-text-light)]"
+              : "text-[var(--color-text-primary)]"
+          } cursor-pointer font-bold hover:text-[var(--color-text-light)] text-lg before:absolute before:content-[""] ${
+            selectedTab == "reviews" && "before:w-full"
+          } before:h-[3px] before:rounded-xl before:bg-[var(--color-text-light)] before:top-[calc(100%-2px)] before:left-0 before:transition-all before:duration-300`}
+          onClick={() => setSelectedTab("reviews")}
+        >
+          Reviews
+        </p>
+      </div>
+
+      <div className="w-full min-h-48 rounded-lg border border-solid border-[var(--color-card-border)]">
+        {selectedTab === "users" && (
+          <>
+            <div className="flex items-center p-4 text-[var(--color-text-light)] font-bold bg-[#26211c] rounded-lg border-b border-solid border-b-[var(--color-card-border)]">
+              <p className="flex-1">User</p>
+              <p className="flex-1">Name</p>
+              <p className="flex-1">Skills</p>
+              <p className="flex-1">Reviews</p>
+              <p className="flex-1">Actions</p>
+            </div>
+            <div className="flex items-center p-4 text-[var(--color-text-light)] font-bold rounded-lg border-b border-solid border-b-[var(--color-card-border)]">
+              <div className="flex-1"><img src={Avat} alt="image" className="block rounded-full w-12 h-12 object-cover" /></div>
+              <p className="flex-1">John Doe</p>
+              <p className="flex-1 text-[var(--color-text-primary)]">Photography, writing</p>
+              <p className="flex-1 text-[var(--color-text-primary)]">5</p>
+              <div className="flex-1 text-[var(--color-text-primary)]"><span className="cursor-pointer transition-all duration-300 hover:text-[var(--color-text-light)]">Edit</span> | <span className="cursor-pointer transition-all duration-300 hover:text-[var(--color-text-light)]">Delete</span></div>
+            </div>
+            <div className="flex items-center p-4 text-[var(--color-text-light)] font-bold rounded-lg border-b border-solid border-b-[var(--color-card-border)]">
+              <div className="flex-1"><img src={Avat} alt="image" className="block rounded-full w-12 h-12 object-cover" /></div>
+              <p className="flex-1">John Doe</p>
+              <p className="flex-1 text-[var(--color-text-primary)]">Photography, writing</p>
+              <p className="flex-1 text-[var(--color-text-primary)]">5</p>
+              <div className="flex-1 text-[var(--color-text-primary)]"><span className="cursor-pointer transition-all duration-300 hover:text-[var(--color-text-light)]">Edit</span> | <span className="cursor-pointer transition-all duration-300 hover:text-[var(--color-text-light)]">Delete</span></div>
+            </div>
+            <div className="flex items-center p-4 text-[var(--color-text-light)] font-bold rounded-lg border-b border-solid border-b-[var(--color-card-border)]">
+              <div className="flex-1"><img src={Avat} alt="image" className="block rounded-full w-12 h-12 object-cover" /></div>
+              <p className="flex-1">John Doe</p>
+              <p className="flex-1 text-[var(--color-text-primary)]">Photography, writing</p>
+              <p className="flex-1 text-[var(--color-text-primary)]">5</p>
+              <div className="flex-1 text-[var(--color-text-primary)]"><span className="cursor-pointer transition-all duration-300 hover:text-[var(--color-text-light)]">Edit</span> | <span className="cursor-pointer transition-all duration-300 hover:text-[var(--color-text-light)]">Delete</span></div>
+            </div>
+            <div className="flex items-center p-4 text-[var(--color-text-light)] font-bold rounded-lg border-b border-solid border-b-[var(--color-card-border)]">
+              <div className="flex-1"><img src={Avat} alt="image" className="block rounded-full w-12 h-12 object-cover" /></div>
+              <p className="flex-1">John Doe</p>
+              <p className="flex-1 text-[var(--color-text-primary)]">Photography, writing</p>
+              <p className="flex-1 text-[var(--color-text-primary)]">5</p>
+              <div className="flex-1 text-[var(--color-text-primary)]"><span className="cursor-pointer transition-all duration-300 hover:text-[var(--color-text-light)]">Edit</span> | <span className="cursor-pointer transition-all duration-300 hover:text-[var(--color-text-light)]">Delete</span></div>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  )
+}
