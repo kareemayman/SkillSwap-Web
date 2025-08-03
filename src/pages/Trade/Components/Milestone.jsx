80f1eb3ee5ad9f0dedcc8d65d83125db731af86e
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2025-08-03 23:54:47 +0300

    feat(edit milestone modal): finish edit milestone modal and logic

--- src/pages/Trade/Components/Milestone.jsx
+++ src/pages/Trade/Components/Milestone.jsx
@@ -8,9 +8,11 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useEffect, useState } from "react"
 import { updateMilestone, updateTrade } from "../../../utils/firestoreUtil"
+import MilestoneModal from "./MilestoneModal"
 
 export default function Milestone({ tradeId, milestone, controls, setTrade, trade, isUserA }) {
   const [myMilestone, setMyMilestone] = useState(milestone)
+  const [showModal, setShowModal] = useState(false)
 
   useEffect(() => {
     updateMilestone(tradeId, myMilestone, isUserA)
@@ -57,62 +59,73 @@ export default function Milestone({ tradeId, milestone, controls, setTrade, trad
   }
 
   return (
-    <div
-      className={`mb-6 p-4 pr-6 rounded-lg border border-[var(--color-card-border)] bg-[#252321] flex items-start gap-3 relative`}
-    >
-      {controls ? (
-        <div
-          onClick={() => {
-            setMyMilestone({ ...myMilestone, isCompleted: !myMilestone.isCompleted })
-          }}
-          className={`rounded-md w-7 h-7 ${
-            myMilestone.isCompleted ? "bg-[var(--main-color)]" : "bg-white"
-          } cursor-pointer transition-all duration-300 flex items-center justify-center`}
-        >
-          <FontAwesomeIcon icon={faCheck} className="text-white"></FontAwesomeIcon>
-        </div>
-      ) : (
-        <div
-          className={`rounded-full ${
-            myMilestone.isCompleted
-              ? "bg-[var(--main-color)] text-white"
-              : "bg-[#36322f] text-[var(--color-text-primary)]"
-          } w-7 h-7 flex items-center justify-center`}
-        >
-          {myMilestone.isCompleted ? (
-            <FontAwesomeIcon icon={faCheck}></FontAwesomeIcon>
-          ) : (
-            <FontAwesomeIcon icon={faFlagCheckered}></FontAwesomeIcon>
+    <>
+      {showModal && (
+        <MilestoneModal
+          data={myMilestone}
+          setShowModal={setShowModal}
+          deleteMilestone={deleteMilestone}
+          setMyMilestone={setMyMilestone}
+        ></MilestoneModal>
+      )}
+      <div
+        className={`mb-6 p-4 pr-6 rounded-lg border border-[var(--color-card-border)] bg-[#252321] flex items-start gap-3 relative`}
+      >
+        {controls ? (
+          <div
+            onClick={() => {
+              setMyMilestone({ ...myMilestone, isCompleted: !myMilestone.isCompleted })
+            }}
+            className={`rounded-md w-7 h-7 ${
+              myMilestone.isCompleted ? "bg-[var(--main-color)]" : "bg-white"
+            } cursor-pointer transition-all duration-300 flex items-center justify-center`}
+          >
+            <FontAwesomeIcon icon={faCheck} className="text-white"></FontAwesomeIcon>
+          </div>
+        ) : (
+          <div
+            className={`rounded-full ${
+              myMilestone.isCompleted
+                ? "bg-[var(--main-color)] text-white"
+                : "bg-[#36322f] text-[var(--color-text-primary)]"
+            } w-7 h-7 flex items-center justify-center`}
+          >
+            {myMilestone.isCompleted ? (
+              <FontAwesomeIcon icon={faCheck}></FontAwesomeIcon>
+            ) : (
+              <FontAwesomeIcon icon={faFlagCheckered}></FontAwesomeIcon>
+            )}
+          </div>
+        )}
+        <div className="flex-1">
+          <h2 className={`text-[var(--color-text-light)] font-bold ${controls && "pr-11"}`}>
+            {myMilestone.title}
+          </h2>
+          <p className="text-[var(--color-text-primary)] font-semibold mt-1">
+            {myMilestone.description}
+          </p>
+          {myMilestone.AI && (
+            <div className="bg-[#31292a] px-1 py-[2px] flex gap-1 items-center text-[var(--main-color)] w-fit text-xs rounded-sm mt-2">
+              <FontAwesomeIcon icon={faRobot}></FontAwesomeIcon>
+              <p>AI Generated</p>
+            </div>
           )}
         </div>
-      )}
-      <div className="flex-1">
-        <h2 className={`text-[var(--color-text-light)] font-bold ${controls && "pr-11"}`}>
-          {myMilestone.title}
-        </h2>
-        <p className="text-[var(--color-text-primary)] font-semibold mt-1">
-          {myMilestone.description}
-        </p>
-        {myMilestone.AI && (
-          <div className="bg-[#31292a] px-1 py-[2px] flex gap-1 items-center text-[var(--main-color)] w-fit text-xs rounded-sm mt-2">
-            <FontAwesomeIcon icon={faRobot}></FontAwesomeIcon>
-            <p>AI Generated</p>
+        {controls && (
+          <div className="flex absolute top-4 right-4 items-center gap-2 text-[var(--color-text-primary)]">
+            <FontAwesomeIcon
+              onClick={() => setShowModal(true)}
+              icon={faPencil}
+              className="cursor-pointer transition-all duration-300 hover:text-[var(--color-text-light)]"
+            ></FontAwesomeIcon>
+            <FontAwesomeIcon
+              onClick={deleteMilestone}
+              icon={faTrash}
+              className="cursor-pointer transition-all duration-300 hover:text-[var(--main-color)]"
+            ></FontAwesomeIcon>
           </div>
         )}
       </div>
-      {controls && (
-        <div className="flex absolute top-4 right-4 items-center gap-2 text-[var(--color-text-primary)]">
-          <FontAwesomeIcon
-            icon={faPencil}
-            className="cursor-pointer transition-all duration-300 hover:text-[var(--color-text-light)]"
-          ></FontAwesomeIcon>
-          <FontAwesomeIcon
-            onClick={deleteMilestone}
-            icon={faTrash}
-            className="cursor-pointer transition-all duration-300 hover:text-[var(--main-color)]"
-          ></FontAwesomeIcon>
-        </div>
-      )}
-    </div>
+    </>
   )
 }
